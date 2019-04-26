b107518fca5c618ba3c7d122f26c47b38bb4a81a
Krycha2341 <krycha2341@gmail.com>
2019-04-26 15:42:12 +0200

    basically nothing

--- src/main/java/com/springboot/springbootapp/service/PharmacyService.java
+++ src/main/java/com/springboot/springbootapp/service/PharmacyService.java
@@ -21,8 +21,8 @@ public class PharmacyService {
     }
 
     public List<Medicine> getMedicineList(){
-        //return medicineList;
         return pharmacyDAO.findAll();
+
     }
 
     public Medicine getDrugById(int medicineId){
