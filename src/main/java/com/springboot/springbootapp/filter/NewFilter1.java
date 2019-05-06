3309b90e6b803879a4380a7aca0d6cf4ce5d40b5
Krycha2341 <krycha2341@gmail.com>
2019-05-06 11:37:14 +0200

    whitetable error msg custom

---
+++ src/main/java/com/springboot/springbootapp/filter/NewFilter1.java
@@ -0,0 +1,18 @@
+package com.springboot.springbootapp.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class NewFilter1 implements Filter{
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("idk why this is here");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+}
