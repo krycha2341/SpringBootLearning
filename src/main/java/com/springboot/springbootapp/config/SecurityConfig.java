461095571b529454e45403c2873bc0213114564e
Krycha2341 <krycha2341@gmail.com>
2019-05-09 13:50:10 +0200

    security

---
+++ src/main/java/com/springboot/springbootapp/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.springboot.springbootapp.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        User.UserBuilder userBuilder = User.builder().passwordEncoder(encoder::encode);
+
+        UserDetails user = userBuilder
+                .username("krycha2341")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+
+    }
+}
