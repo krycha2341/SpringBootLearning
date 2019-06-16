a821c104c22802e235c317ed614bedff98f0531c
Krycha2341 <krycha2341@gmail.com>
2019-06-16 19:19:49 +0200

    commit

--- src/main/java/com/springboot/springbootapp/SpringbootappApplication.java
+++ src/main/java/com/springboot/springbootapp/SpringbootappApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+
 public class SpringbootappApplication {
 
 		public static void main(String[] args) {
